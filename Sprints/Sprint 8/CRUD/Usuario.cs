10476e10b9856da4590fc10f0ad2ec1a00d6e193
Duartexz <163935340+Duartexz@users.noreply.github.com>
2024-05-24 20:08:12 -0300

    Usuario

---
+++ Sprints/Sprint 8/CRUD/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int CPF { get; set; }
+
+        public byte Contato { get; set; }
+    }
+}
