80c6d3ecd0a8c15c3e57812516e3a89ce4e8eea7
Duartexz <163935340+Duartexz@users.noreply.github.com>
2024-05-24 20:05:56 -0300

    Funcionario

---
+++ Sprints/Sprint 8/CRUD/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1555
+{
+    class Funcionario : Usuario
+    {
+        public char Status { get; set; }
+    }
+}
