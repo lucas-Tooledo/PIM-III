129feec1e0410bc76ca6f41348ceecf75010dc14
Duartexz <163935340+Duartexz@users.noreply.github.com>
2024-05-24 20:06:48 -0300

    Producao

---
+++ Sprints/Sprint 8/CRUD/Producao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1555
+{
+    class Producao
+    {
+        private int ID { get; set; }
+
+        private int DataInicio { get; set; }
+
+        private int Prazo { get; set; }
+
+        private int Qtd { get; set; }
+
+        private int DataFim { get; set; }
+
+        private char Prioridade { get; set; }
+
+        private char Tipo { get; set; }
+
+        private int ID_Fornecedor { get; set; }
+
+        private int Código_Produto { get; set; }
+
+        private int Codigo_Cliente { get; set; }
+
+        private int N_etapas { get; set; }
+    }
+}
